d60d058bce68e3a4d81f30e94e8c96168d6dec67
chenhao1umbc <chenhao1@umbc.edu>
2022-11-03 16:20:50 -0400

    the distribution plots are finished

--- distributions.py
+++ distributions.py
@@ -20,45 +20,50 @@ print('starting date time ', datetime.now())
 
 
 #%%
-loga = torch.tensor(0)
-beta, gamma, zeta, eps = torch.tensor([2/3, -0.1, 1.1, 1e-20])
-u = torch.arange(0,1,1e-3)
-s = torch.sigmoid((torch.log(u+eps) - torch.log(1 - u+eps) + loga) / beta)
-sbar = s * (zeta - gamma) + gamma
-z = hard_sigmoid(sbar)
+def plot_pdf(loga):
+    beta, gamma, zeta, eps = torch.tensor([0.5, -0.1, 1.1, 1e-20])
+    u = torch.arange(0,1,1e-3)+eps
+    s = torch.sigmoid((torch.log(u) - torch.log(1 - u) + loga) / beta)
+    sbar = s * (zeta - gamma) + gamma
+    z = hard_sigmoid(sbar)
 
-#%% Concrete pdf and cdf
-def pdf_qs(x):
-    qs = beta*loga.exp()*x**(-beta-1)*(1-x)**(-beta-1)\
-    /(loga.exp()*x**(-beta)+(1-x)**(-beta))**2
-    return qs
-def cdf_Qs(x):
-    Qs = ((x.log()-(1-x).log())*beta-loga).sigmoid()
-    return Qs
-qs = pdf_qs(s)
-Qs = cdf_Qs(s)
-plt.plot(u, qs)
-plt.plot(u, Qs)
+    "Concrete pdf and cdf"
+    def pdf_qs(x):
+        qs = beta*loga.exp()*x**(-beta-1)*(1-x)**(-beta-1)\
+        /(loga.exp()*x**(-beta)+(1-x)**(-beta))**2
+        return qs
+    def cdf_Qs(x):
+        Qs = ((x.log()-(1-x).log())*beta-loga).sigmoid()
+        return Qs
+    qs = pdf_qs(s)
+    Qs = cdf_Qs(s)
 
-#%% Hard concrete pdf and cdf
-def pdf_qs_bar(x):
-    return 1/(zeta-gamma).abs()*pdf_qs((x-gamma)/(zeta-gamma))
+    "Hard concrete pdf and cdf"
+    def pdf_qs_bar(x):
+        return 1/(zeta-gamma).abs()*pdf_qs((x-gamma)/(zeta-gamma))
 
-def cdf_Qs_bar(x):
-    return cdf_Qs((x-gamma)/(zeta-gamma))
+    def cdf_Qs_bar(x):
+        return cdf_Qs((x-gamma)/(zeta-gamma))
 
-def delta(x):
-    a = x.clone()
-    a[a!=0] = 1
-    a = 1-a
-    return a
+    qz = z.clone()
+    qz[z==0] = cdf_Qs_bar(0)
+    qz[z==1] = (1-cdf_Qs_bar(1))
+    idx = torch.logical_or(z==0, z==1)
+    qz[~idx] = (cdf_Qs_bar(1)-cdf_Qs_bar(0)) * pdf_qs_bar(z[~idx])
+    "l0 norm paper fig.2 was wrong as below, since it did not multiply the coeff."
+    # qz[~idx] = pdf_qs_bar(z[~idx]) 
 
-qz1 = cdf_Qs_bar(0)*delta(z) + (1-cdf_Qs_bar(1))*delta(z-1) + \
-    (cdf_Qs_bar(1)-cdf_Qs_bar(0))*pdf_qs_bar(z)
-qz = z.clone()
-qz[z==0] = cdf_Qs_bar(0)
-qz[z==1] = (1-cdf_Qs_bar(1))
-idx = torch.logical_or(z==0, z==1)
-qz[~idx] = (cdf_Qs_bar(1)-cdf_Qs_bar(0))*\
-    pdf_qs_bar(z[~idx])
+    s0 = max((z==0).sum()/z.shape[0]*20, 1)
+    s1 = max((z==1).sum()/z.shape[0]*20, 1)
+    plt.plot(s, qs, '--', color="#1f77b4") 
+    plt.plot(sbar, pdf_qs_bar(sbar), '-.', color="#2ca02c")
+    plt.plot(z[~idx], qz[~idx], color="#ff7f0e")
+    plt.plot(1,  1-cdf_Qs_bar(1), 'o', markersize=s1, color="#ff7f0e")
+    plt.plot(0, cdf_Qs_bar(0), 'o', markersize=s0, color="#ff7f0e")
+    plt.grid()
+    plt.ylim([0,2.5])
+    plt.legend(['Concrete', 'Streched concrete', 'Hard concrete', 'Hard concrete p(z=0) and p(z=1) '])
+    plt.show()
 
+loga = torch.tensor(-3)
+plot_pdf(loga)
