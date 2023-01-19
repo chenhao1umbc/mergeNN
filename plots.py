3b6419d89063a9a9e0f9e6c7c201d895bef6e582
chenhao1umbc <chenhao1@umbc.edu>
2023-01-19 15:58:42 -0500

    updated barchart

--- plots.py
+++ plots.py
@@ -96,24 +96,16 @@ plt.tight_layout() # otherwise the text will be chopped off
 plt.savefig('module_loga.png')
 
 #%%
-h = [328.9614458622783,
- 331.01736405892507,
- 328.14458622783,
- 330.736405892507,
- 329.9614458622783,
- 274.6798730743995,
- 272.42975964690595,
- 273.6798730743995,
- 239.56801906769252,
- 238.56801906769252,
- 238.56801906769252]
+h = [0.1606, 0.1744, 0.5225, 0.5387, 0.5587, 0.8438, 0.8456, 0.8506, 0.9413, 0.9581, 0.9581]
 plt.figure(figsize=(10,5))
-for i in range(4):
-    rgb = torch.rand(3).tolist()
-    if i == 0: plt.bar([str(ii) for ii in range(1, 6)], h[:5], width=0.6, color=[rgb])
-    if i == 1: plt.bar([str(ii) for ii in range(6, 9)], h[5:8], width=0.6, color=[rgb])
-    if i == 2: plt.bar([str(9), str(10)], h[8:10], width=0.6, color=[rgb])
-    if i == 3: plt.bar('Merged', h[10], width=0.6, color=[rgb])
+# for i in range(4):
+#     rgb = torch.rand(3).tolist()
+#     if i == 0: plt.bar([str(ii) for ii in range(1, 6)], h[:5], width=0.6, color=[rgb])
+#     if i == 1: plt.bar([str(ii) for ii in range(6, 9)], h[5:8], width=0.6, color=[rgb])
+#     if i == 2: plt.bar([str(9), str(10)], h[8:10], width=0.6, color=[rgb])
+#     if i == 3: plt.bar('Merged', h[10], width=0.6, color=[rgb])
+plt.bar([str(i) for i in range(1, 11)], h[:10])
+plt.bar('Merged', h[10], color='#ff7f0e')
 plt.xlabel('Model index')
-plt.ylabel('PPL')
+plt.ylabel('Accuracy')
 plt.savefig('bar.png')
