423294ef037e24e37b12144989d4fa12ba3dcede
chenhao1umbc <chenhao1@umbc.edu>
2023-01-20 15:42:39 -0500

    added train vae code

--- train_vae.py
+++ train_vae.py
@@ -1,118 +1,154 @@
-"""This cell is train_cifar0.py
-This file is using CIFAR100, 
-Resnet18 or shufflenet, which can be changed in the load model section
 """
-#%% load dependency
-import os
+This file is based on the https://github.com/jundsp/VAE-BSS
+Users should first git clone the repo,
+then put this file in the file and run.
+
+(do not run it in this folder, it pops up errors)
+"""
+
+
+#%%
 import numpy as np
-from tqdm import tqdm
-from utils import *
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
-from skimage.io import imread
-from datetime import datetime
-print('starting date time ', datetime.now())
+import torch
+import torch.utils.data
+from torch import nn, optim
+from torch.nn import functional as F
+import torchvision
+from torchvision import datasets, transforms
+from torchvision.utils import save_image, make_grid
+import matplotlib.pyplot as plt
+from src.model import *
+from src.argparser import *
+from src.utils import *
+from src.dataloader import *
 
-#%% prepare data
-train_dataset = datasets.MNIST(root='data', 
-                               train=True, 
-                               transform=transforms.ToTensor(),
-                               download=True)
+#%%
+def train(epoch):
 
-test_dataset = datasets.MNIST(root='data', 
-                              train=False, 
-                              transform=transforms.ToTensor())
-
-train_loader = DataLoader(dataset=train_dataset, 
-                          batch_size=128, 
-                          shuffle=True)
-
-validation_loader = DataLoader(dataset=test_dataset, 
-                         batch_size=128, 
-                         shuffle=False)
-
-
-#%% load model
-model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
-model.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False)
-model = model.cuda()
-
-#%% train_net
-id = 'mnist' # for diff. runs
-best_validation_accuracy = 0. # used to pick the best-performing model on the validation set
-train_accs = []
-val_accs = []
-
-opt = {'epochs':200}
-optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=1e-4)
-scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150], gamma=0.1)
-
-loss_func = nn.CrossEntropyLoss()
-
-for epoch in range(opt['epochs']):
     model.train()
+    train_losses = torch.zeros(3)
 
-    # print training info
-    print("### Epoch {}:".format(epoch))
-    total_train_examples = 0
-    num_correct_train = 0
+    for batch_idx, (data,_) in enumerate(train_loader):
+        data = mix_data(data.to(device))[0].view(-1,dimx)
 
-    for batch_index, (inputs, gt_label) in enumerate(train_loader):
-        inputs = inputs.cuda()
-        gt_label = gt_label.cuda()
         optimizer.zero_grad()
-        predictions = model(inputs)
-        loss = loss_func(predictions, gt_label)
+        recon_y, mu_z, logvar_z, _ = model(data)
+        loss, ELL, KLD = loss_function(data,recon_y, mu_z, logvar_z, beta=beta)
         loss.backward()
         optimizer.step()
 
-        _, predicted_class = predictions.max(1)
-        total_train_examples += predicted_class.size(0)
-        num_correct_train += predicted_class.eq(gt_label).sum().item()
+        train_losses[0] += loss.item()
+        train_losses[1] += ELL.item()
+        train_losses[2] += KLD.item()
 
-    # get results
-    train_acc = num_correct_train / total_train_examples
-    print("Training accuracy: {}".format(train_acc))
-    train_accs.append(train_acc)
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)] \t -ELL: {:5.6f} \t KLD: {:5.6f} \t Loss: {:5.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                ELL.item() / len(data),KLD.item() / len(data),loss.item() / len(data)))
 
-    # evaluation
-    total_val_examples = 0
-    num_correct_val = 0
+    train_losses /= len(train_loader.dataset)
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_losses[0]))
+
+    return train_losses
+
+def test(epoch):
+
     model.eval()
-    with torch.no_grad(): # don't save parameter gradients/changes since this is not for model training
-        for batch_index, (inputs, gt_label) in enumerate(validation_loader):
-            inputs = inputs.cuda()
-            gt_label = gt_label.cuda()
-            predictions = model(inputs)
+    test_losses = torch.zeros(3)
 
-            _, predicted_class = predictions.max(1)
-            total_val_examples += predicted_class.size(0)
-            num_correct_val += predicted_class.eq(gt_label).sum().item()
+    with torch.no_grad():
+        for i, (data,_) in enumerate(test_loader):
+            data = mix_data(data.to(device))[0].view(-1,dimx)
 
-        # get validation results
-        val_acc = num_correct_val / total_val_examples
-        print("Validation accuracy: {}".format(val_acc))
-        val_accs.append(val_acc)
+            recon_y, mu_z, logvar_z, recons = model(data)
+            loss, ELL, KLD = loss_function(data,recon_y, mu_z, logvar_z, beta=beta)
 
-    # Finally, save model if the validation accuracy is the best so far
-    if val_acc > best_validation_accuracy:
-        best_validation_accuracy = val_acc
-        print("Validation accuracy improved; saving model.")
-        torch.save(model.state_dict(), f'resnet18{id}_dict.pt')
-    scheduler.step()
+            test_losses[0] += loss.item()
+            test_losses[1] += ELL.item()
+            test_losses[2] += KLD.item()
 
-#%% plot train and val results
-epochs_list = list(range(opt['epochs']))
-plt.figure()
-plt.plot(epochs_list, train_accs, 'b-', label='training set accuracy')
-plt.plot(epochs_list, val_accs, 'r-', label='validation set accuracy')
-plt.xlabel('epoch')
-plt.ylabel('prediction accuracy')
-plt.ylim(0.01, 1)
-plt.title('Classifier training evolution:\nprediction accuracy over time')
-plt.legend()
-plt.savefig(f'train_val{id}.png')
-plt.show()
+        n = min(data.size(0), 6)
+        ncols = (2+args.sources)
+        comparison = torch.zeros(n*ncols,1,28,28)
+        comparison[::ncols] = data.view(data.size(0), 1, 28, 28)[:n]
+        comparison[1::ncols] = recon_y.view(data.size(0), 1, 28, 28)[:n]
+        for i in range(args.sources):
+            comparison[(i+2)::ncols] = recons[:,i].view(data.size(0), 1, 28, 28)[:n]
 
-print('done')
-print('End date time ', datetime.now())
+        grid = make_grid(comparison,nrow=ncols)
+
+        save_image(comparison.cpu(),'results/reconstruction_' + str(epoch) + '.png', nrow=ncols)
+
+        test_losses /= len(test_loader.dataset)
+        print('====> Test set loss: {:.4f}'.format(test_losses[0]))
+
+    return test_losses
+
+def plot_losses(losses):
+	plt.figure()
+	plt.plot(np.array(range(1,args.epochs+1)),losses["train"][:,0].view(-1),label="Train")
+	plt.plot(np.array(range(1,args.epochs+1)),losses["test"][:,0].view(-1),label="Test")
+	plt.xlabel('Epoch'), plt.ylabel('Loss'), plt.legend(), plt.xlim(1,args.epochs)
+	plt.savefig('results/losses.png')
+	plt.close()
+
+
+# args = parser.parse_args()
+# torch.manual_seed(args.seed)
+class Arg():
+    def __init__(self) -> None:
+        pass
+args = Arg()
+args.seed = 1
+args.num_workers = 4
+args.data_directory = 'data'
+args.batch_size = 64
+args.dimz = 20
+args.sources = 2 # number of sources
+args.variational = True
+args.learning_rate = 2e-4
+args.decay = 0.9998
+args.epochs = 10
+args.save_interval = 1
+args.prior = 0.5
+args.scale = 'gauss'
+args.warm_up = 50
+args.beta_max = 0.5
+args.log_interval = 10
+
+#%%
+torch.manual_seed(args.seed)
+args.cuda = True
+device = torch.device("cuda" if args.cuda else "cpu")
+kwargs = {'num_workers': args.num_workers, 'pin_memory': True} if args.cuda else {}
+
+train_loader, test_loader = get_data_loaders(args.data_directory,args.batch_size,kwargs)
+
+# MNIST is 28 X 28
+dimx = int(28*28)
+
+model = VAE(dimx=dimx,dimz=args.dimz,n_sources=args.sources,device=device,variational=args.variational).to(device)
+loss_function = Loss(sources=args.sources,likelihood='laplace',variational=args.variational,prior=args.prior,scale=args.scale)
+
+optimizer = optim.Adam(model.parameters(), lr = args.learning_rate)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=args.decay, last_epoch=-1)
+
+losses = {"train": torch.zeros(args.epochs,3), "test": torch.zeros(args.epochs,3)}
+
+for epoch in range(1, args.epochs+1):
+    beta = min(1.0,(epoch)/min(args.epochs,args.warm_up)) * args.beta_max
+
+    losses["train"][epoch-1] = train(epoch)
+    losses["test"][epoch-1] = test(epoch)
+
+    if optimizer.param_groups[0]['lr'] >= 1.0e-5:
+        scheduler.step()
+
+    with torch.no_grad():
+        if epoch % args.save_interval == 0:
+            torch.save(model.state_dict(),'saves/model_'+('vae' if args.variational else 'ae')+'_K' + str(args.sources) +  '.pt')
+
+
+plot_losses(losses)
